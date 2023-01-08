f6dcb6f2cd34c71490dd47d84d06bdffc27a3902
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-01-08 17:48:17 +0900

    プッシュ

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -24,24 +24,14 @@ VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOOR
 	float4 wnormal = normalize(mul(world,float4(normal,0)));
 	float4 wpos = mul(world,pos);
 
-	//環境反射光
-	float3 ambient = m_ambient;
-	//拡散反射光
-	float3 diffuse = dot(lightv, wnormal.xyz)* m_diffuse;
-	//光沢度
-	float shininess = 4.0f;
-	float3 eyedir = normalize(cameraPos- wpos.xyz);
-	//反射光ベクトル
-	float3 reflect = normalize(-lightv + 2 * dot(lightv, wnormal.xyz) * wnormal.xyz);
-	//鏡面反射光
-	float3 specular = pow(saturate(dot(reflect,eyedir)),shininess)*m_specular;
 	
 	VSOutput output;
 	output.svpos = mul(mul(viewproj,world), pos);
 	//Lambart反射の計算
-	output.color.rgb = (ambient+diffuse+specular) * lightColor;
-	output.color.a = m_alpha;
+	output.worldpos = wpos;
+	output.normal = wnormal.xyz;
 	output.uv = uv;
+
 	return output;
 
 }
